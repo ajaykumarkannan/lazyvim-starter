42491730f7fc4433043cba8ee103009e3ab4eb83
Ajaykumar Kannan <kannan.ajay@gmail.com>
2026-03-03 16:56:26 -0500

    Created customizations for Ajay

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Buffer navigation (from .vimrc)
+vim.keymap.set("n", "<leader>z", "<cmd>bp<cr>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<leader>x", "<cmd>bn<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>c", "<cmd>bd<cr>", { desc = "Close Buffer" })
+
+-- Save with leader+s (from .vimrc)
+vim.keymap.set("n", "<leader>s", "<cmd>w<cr>", { desc = "Save File" })
+
+-- Split windows (from .vimrc)
+vim.keymap.set("n", "<leader>h", "<cmd>split<cr>", { desc = "Horizontal Split" })
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "Vertical Split" })
+
+-- Set working directory to current file (from .vimrc)
+vim.keymap.set("n", "<leader>.", "<cmd>lcd %:p:h<cr>", { desc = "Set CWD to file dir" })
+
+-- Command abbreviations for typos (from .vimrc)
+vim.cmd("cnoreabbrev W! w!")
+vim.cmd("cnoreabbrev Q! q!")
+vim.cmd("cnoreabbrev Wq wq")
+vim.cmd("cnoreabbrev wQ wq")
+vim.cmd("cnoreabbrev WQ wq")
+vim.cmd("cnoreabbrev W w")
+vim.cmd("cnoreabbrev Q q")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.mapleader = ","
+
+-- Word wrap settings (from .vimrc)
+vim.opt.breakindent = true -- Indent word-wrapped lines as much as parent
+vim.opt.linebreak = true -- Don't split words when wrapping
+
+-- Disable swap/backup (from .vimrc)
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Search centering (n/N will center the match on screen)
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev search result (centered)" })
+
+-- Scrolloff - keep 3 lines visible above/below cursor
+vim.opt.scrolloff = 3
