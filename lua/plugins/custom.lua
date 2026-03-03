1428762bf913d3a57e3c458b08b048e91a119791
Ajaykumar Kannan <kannan.ajay@gmail.com>
2026-03-03 17:01:44 -0500

    Update lazy vim config

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,42 @@
+-- Custom plugins migrated from .vimrc
+return {
+  -- LLVM and TableGen syntax highlighting
+  {
+    "rhysd/vim-llvm",
+    ft = { "llvm", "tablegen" },
+  },
+
+  -- Strip trailing whitespace on save
+  -- Using autocmd since conform.nvim handles formatting
+  {
+    "LazyVim/LazyVim",
+    opts = function()
+      -- Set up LLVM/TableGen file types
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = "*.ll",
+        callback = function()
+          vim.bo.filetype = "llvm"
+        end,
+      })
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = "*.td",
+        callback = function()
+          vim.bo.filetype = "tablegen"
+        end,
+      })
+
+      -- Strip trailing whitespace on save (for all files)
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function()
+          -- Save cursor position
+          local cursor = vim.api.nvim_win_get_cursor(0)
+          -- Remove trailing whitespace
+          vim.cmd([[%s/\s\+$//e]])
+          -- Restore cursor position
+          pcall(vim.api.nvim_win_set_cursor, 0, cursor)
+        end,
+      })
+    end,
+  },
+}
